2dae7b264783d578c5947ae7b1131e5ee00c56eb
bohan2345 <bohan.zheng@gmail.com>
2014-06-04 15:56:53 -0700

    learn collection

---
+++ learn_collection.js
@@ -0,0 +1,38 @@
+//create a collection class
+var TodoList = Backbone.Collection.extend({
+	model : TodoItem
+});
+
+//new instance
+var todoList = new TodoList();
+
+todoList.length;
+todoList.add(todoItem);
+todoList.at(1);
+todoList.get(1);
+todoList.remove(todoItem);
+
+//reset the todoList instance with a list of new models
+todoList.reset(a_list_of_new_models);
+
+//fetch data from server
+todoList.url = "/models";
+todoList.fetch();
+
+//add event listener
+todoList.on("add", function() {
+});
+
+//iteration (call the function for each item)
+todoList.foreach(function(todoItem) {
+});
+
+//map function
+todoList.map(function(todoItem) {
+	return todoItem.get("id");
+});
+
+//filter models by some criteria, the return of the function should be a boolean expression
+todoList.filter(function(todoItem) {
+	return todoItem.get("id") > 2;
+});
